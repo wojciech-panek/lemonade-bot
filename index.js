10a7103caa726e6f46b989dc8f183755c37c42bd
Wojciech Panek <wpanek@apptension.com>
2019-08-05 10:38:11 +0200

    Implement webapp

--- index.js
+++ index.js
@@ -1,38 +1,51 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const { WebClient } = require('@slack/web-api');
 
 const tokens = [
   'xoxp-2612359521-51100563574-650242558917-9b2c8fc9d19ecafac7098175ca2c8071',
-  'xoxp-2612359521-49396721797-638850536563-94822bd72b054c48356a11ce77d0f686'
+  'xoxp-2612359521-49396721797-638850536563-94822bd72b054c48356a11ce77d0f686',
 ];
 // const channelId = 'GCN1Z9N91';
 const channelId = 'GLU8VEG7P';
+const password = 'Lemonade321';
 
 const deleteMessage = async (message, client, user) => {
   try {
     if (message.replies && message.replies.length) {
-      await Promise.all(message.replies.map((message) => deleteMessage(message, client, user)))
+      await Promise.all(
+        message.replies.map(message => deleteMessage(message, client, user))
+      );
     }
 
     if (message.user === user.user_id) {
-      await client.chat.delete({ channel: channelId, ts: message.ts, as_user: true });
+      await client.chat.delete({
+        channel: channelId,
+        ts: message.ts,
+        as_user: true,
+      });
+      return Promise.resolve(true);
     }
-    return Promise.resolve();
+    return Promise.resolve(false);
   } catch (e) {
     // if something goes wrong - silent fail
-    return Promise.resolve();
+    return Promise.resolve(false);
   }
 };
 
-const getMessages = async (client) => {
+const getMessages = async client => {
   let messages = [];
   let hasMore = true;
   let cursor = null;
 
   while (hasMore) {
     try {
-      const response = await client.conversations.history({ limit: 5, channel: channelId, cursor });
+      const response = await client.conversations.history({
+        limit: 5,
+        channel: channelId,
+        cursor,
+      });
 
       messages = messages.concat(response.messages);
       hasMore = response.has_more;
@@ -46,22 +59,38 @@ const getMessages = async (client) => {
 };
 
 const clearHistory = async (req, res) => {
-  await Promise.all(tokens.map(async token => {
-    const client = new WebClient(token);
-    const user = await client.auth.test();
+  if (req.body.password !== password) {
+    return res.status(401).send({ status: 'error' });
+  }
 
-    const messages = await getMessages(client);
+  const deletedCounts = await Promise.all(
+    tokens.map(async token => {
+      const client = new WebClient(token);
+      const user = await client.auth.test();
 
-    return Promise.all(messages.map((message) => deleteMessage(message, client, user)));
-  }));
+      const messages = await getMessages(client);
 
-  res.send({ status: 'ok' });
+      const deletedMessages = await Promise.all(
+        messages.map(message => deleteMessage(message, client, user))
+      );
+      return Promise.resolve(deletedMessages.filter(value => value).length);
+    })
+  );
+
+  const deletedSum = deletedCounts.reduce((prev, current) => prev + current, 0);
+
+  return res.send({ status: 'ok', deleted: deletedSum });
 };
 
 const app = express();
+const router = express.Router();
+
+router.post('/clear-history', clearHistory);
 
 app.use(cors({ origin: true }));
-app.post('/', clearHistory);
+app.use(bodyParser.json());
+app.use('/api', router);
+app.use('/', express.static('webapp/build'));
 
 const port = process.env.PORT || 3000;
 
